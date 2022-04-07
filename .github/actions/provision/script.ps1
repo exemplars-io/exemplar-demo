781826ea5fde40c25c010db2e2ef209659f51550
Satya Tanwar <satya.tanwar@gmail.com>
2022-04-07 15:12:27 -0400

    updated scripts

--- .github/actions/provision/script.ps1
+++ .github/actions/provision/script.ps1
@@ -1,12 +1,12 @@
-$Repo = "${`{ github.repository }`}"
+$Repo = ${Env:GITHUB_REPOSITORY}
 echo $Repo
 $BaseUri = "https://api.github.com"
 echo $BaseUri
 $ArtifactUri = "$BaseUri/repos/$Repo/actions/artifacts"
 echo $ArtifactUri
-
-$Token = "${`{ github.token }`}" | ConvertTo-SecureString -AsPlainText
-
+$Token = ${Env:GITHUB_TOKEN} | ConvertTo-SecureString -AsPlainText
+echo $Token
+echo ${Env:INPUT_ENCRYPTIONKEY}
 $RestResponse = Invoke-RestMethod -Authentication Bearer -Uri $ArtifactUri -Token $Token | Select-Object -ExpandProperty artifacts
 if ($RestResponse){
   $MostRecentArtifactURI = $RestResponse | Sort-Object -Property created_at -Descending | where name -eq "terraformstatefile" | Select-Object -First 1 | Select-Object -ExpandProperty archive_download_url
@@ -14,23 +14,15 @@ if ($RestResponse){
   if ($MostRecentArtifactURI){
     Invoke-RestMethod -uri $MostRecentArtifactURI -Token $Token -Authentication bearer -outfile ./state.zip
     Expand-Archive state.zip
-    openssl enc -d -in state/terraform.tfstate.enc -aes-256-cbc -pbkdf2 -pass pass:"${`{ inputs.encryptionkey }`}" -out terraform.tfstate
+    openssl enc -d -in state/terraform.tfstate.enc -aes-256-cbc -pbkdf2 -pass pass:${Env:ENCRYPTION_KEY} -out terraform.tfstate
   }
 }
 terraform init
-$terraformapply = "${`{ inputs.apply }`}"
-$custom_plan_flags = "${`{ inputs.custom_plan_flags }`}"
-$custom_apply_flags = "${`{ inputs.custom_apply_flags }`}"
-if ($terraformapply -eq "false"){
-  $terraformapply = $false
-}
-terraform plan $custom_plan_flags
-if ($terraformapply){
-  terraform apply -auto-approve $custom_apply_flags
-}
+terraform plan 
+terraform apply -auto-approve 
 
 $StateExists = Test-Path -Path terraform.tfstate -PathType Leaf
 if ($StateExists){
   echo "state file found, encrypting with the key"
-  openssl enc -in terraform.tfstate -aes-256-cbc -pbkdf2 -pass pass:"${`{ inputs.encryptionkey }`}" -out terraform.tfstate.enc
+  openssl enc -in terraform.tfstate -aes-256-cbc -pbkdf2 -pass pass:${Env:ENCRYPTION_KEY} -out terraform.tfstate.enc
 }   
