a5682ac90cc822ea783d939e51c2161ca25a3de1
Satya Tanwar <satya.tanwar@gmail.com>
2022-04-07 14:15:21 -0400

    Update script.ps1

--- .github/actions/provision/script.ps1
+++ .github/actions/provision/script.ps1
@@ -4,3 +4,5 @@ $BaseUri = "https://api.github.com"
 echo $BaseUri
 $ArtifactUri = "$BaseUri/repos/$Repo/actions/artifacts"
 echo $ArtifactUri
+$Token = ${Env:GITHUB_TOKEN} | ConvertTo-SecureString -AsPlainText
+echo $Token
