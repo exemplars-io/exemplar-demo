d614421323af33ae3722abb1c4e6f3f26632421b
Satya Tanwar <satya.tanwar@gmail.com>
2022-04-07 14:54:33 -0400

    Update script.ps1

--- .github/actions/provision/script.ps1
+++ .github/actions/provision/script.ps1
@@ -6,7 +6,7 @@ $ArtifactUri = "$BaseUri/repos/$Repo/actions/artifacts"
 echo $ArtifactUri
 $Token = ${Env:GITHUB_TOKEN} | ConvertTo-SecureString -AsPlainText
 echo $Token
-echo ${Env:encryptionkey}
+echo ${Env:INPUT_ENCRYPTIONKEY}
 $RestResponse = Invoke-RestMethod -Authentication Bearer -Uri $ArtifactUri -Token $Token | Select-Object -ExpandProperty artifacts
 if ($RestResponse){
   $MostRecentArtifactURI = $RestResponse | Sort-Object -Property created_at -Descending | where name -eq "terraformstatefile" | Select-Object -First 1 | Select-Object -ExpandProperty archive_download_url
@@ -14,6 +14,6 @@ if ($RestResponse){
   if ($MostRecentArtifactURI){
     Invoke-RestMethod -uri $MostRecentArtifactURI -Token $Token -Authentication bearer -outfile ./state.zip
     Expand-Archive state.zip
-    openssl enc -d -in state/terraform.tfstate.enc -aes-256-cbc -pbkdf2 -pass pass:${Env:encryptionkey} -out terraform.tfstate
+    openssl enc -d -in state/terraform.tfstate.enc -aes-256-cbc -pbkdf2 -pass pass:${Env:INPUT_ENCRYPTIONKEY} -out terraform.tfstate
   }
 }
